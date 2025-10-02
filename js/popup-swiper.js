b786efe407da5fdd4b638342361997d8a0eab982
likeyeon <chy98csy00@naver.com>
2025-10-02 21:40:48 +0900

    ✨ Implement .popup-swiper

---
+++ js/popup-swiper.js
@@ -0,0 +1,44 @@
+import { popupSwiper } from './swiper.js';
+
+const popupSwiperBanner = document.querySelector('.popup');
+const popupSwiperPlayButtonList = popupSwiperBanner.querySelectorAll(
+  '.play-button-wrapper button'
+);
+const popupSwiperPauseButton = popupSwiperBanner.querySelector(
+  '.swiper-button-pause'
+);
+const popupSwiperPlayButton = popupSwiperBanner.querySelector(
+  '.swiper-button-play'
+);
+
+// .swiper-index-last 숫자값 넣기
+const popupSwiperLastIndex =
+  popupSwiperBanner.querySelector('.swiper-index-last');
+const popupSwiperSlideList =
+  popupSwiperBanner.querySelectorAll('.swiper-slide').length;
+popupSwiperLastIndex.textContent = popupSwiperSlideList;
+
+// 슬라이드 중지
+function stoppopupSwiper() {
+  popupSwiperPlayButtonList.forEach((button) => {
+    button.classList.remove('is-active');
+    button.setAttribute('aria-hidden', '');
+  });
+  popupSwiperPlayButton.classList.add('is-active');
+  popupSwiperPlayButton.removeAttribute('aria-hidden');
+  popupSwiper.autoplay.stop();
+}
+
+// 슬라이드 재생
+function playpopupSwiper() {
+  popupSwiperPlayButtonList.forEach((button) => {
+    button.classList.remove('is-active');
+    button.setAttribute('aria-hidden', '');
+  });
+  popupSwiperPauseButton.classList.add('is-active');
+  popupSwiperPauseButton.removeAttribute('aria-hidden');
+  popupSwiper.autoplay.start();
+}
+
+popupSwiperPauseButton.addEventListener('click', stoppopupSwiper);
+popupSwiperPlayButton.addEventListener('click', playpopupSwiper);
--- js/swiper.js
+++ js/swiper.js
@@ -23,4 +23,28 @@ const visualSwiper = new Swiper('.visual-swiper', {
   },
 });
 
+const popupSwiper = new Swiper('.popup-swiper', {
+  loop: true,
+  speed: 300,
+  autoplay: {
+    delay: 7000,
+  },
+
+  navigation: {
+    nextEl: '.popup .swiper-button-next',
+    prevEl: '.popup .swiper-button-prev',
+  },
+
+  on: {
+    slideChange: function () {
+      const currentIdx = this.realIndex + 1;
+      const popupSwiperCurrentIndex = document.querySelector(
+        '.popup .swiper-index-current'
+      );
+      popupSwiperCurrentIndex.innerText = currentIdx;
+    },
+  },
+});
+
 export { visualSwiper };
+export { popupSwiper };
