357cab28beed2a4ff3f40946e04c323bf9ce6289
likeyeon <chy98csy00@naver.com>
2025-10-19 15:43:13 +0900

    🚸 Refactor keydown event in GNB, LNB

---
+++ js/keycode.js
@@ -0,0 +1,5 @@
+export const keydownTab = 9;
+export const keydownEnd = 35;
+export const keydownHome = 36;
+export const keydownLeft = 37;
+export const keydownRight = 39;
--- js/lnb-hover.js
+++ js/lnb-hover.js
@@ -1,4 +1,13 @@
+import {
+  keydownEnd,
+  keydownHome,
+  keydownLeft,
+  keydownRight,
+} from './keycode.js';
+
 const depth1Items = document.querySelectorAll('.gnb .depth1');
+const depth1ItemsTitle = document.querySelectorAll('.gnb .depth1-title');
+const depth1ItemsLength = depth1ItemsTitle.length;
 
 depth1Items.forEach((item) => {
   item.addEventListener('mouseover', function () {
@@ -18,6 +27,39 @@ depth1Items.forEach((item) => {
   item.addEventListener('focusout', function (e) {
     if (!item.contains(e.relatedTarget)) {
       item.classList.remove('over');
+    }
+  });
+});
+
+depth1ItemsTitle.forEach((item, idx) => {
+  item.addEventListener('keydown', function (e) {
+    switch (e.keyCode) {
+      case keydownRight:
+        item.classList.remove('over');
+        if (idx < depth1ItemsLength - 1) {
+          depth1ItemsTitle[idx + 1].focus();
+        } else if (idx === depth1ItemsLength - 1) {
+          depth1ItemsTitle[0].focus();
+        }
+        break;
+      case keydownLeft:
+        item.classList.remove('over');
+        if (idx > 0) {
+          depth1ItemsTitle[idx - 1].focus();
+        } else if (idx === 0) {
+          depth1ItemsTitle[depth1ItemsLength - 1].focus();
+        }
+        break;
+      case keydownEnd:
+        e.preventDefault();
+        item.classList.remove('over');
+        depth1ItemsTitle[depth1ItemsLength - 1].focus();
+        break;
+      case keydownHome:
+        e.preventDefault();
+        item.classList.remove('over');
+        depth1ItemsTitle[0].focus();
+        break;
     }
   });
 });
