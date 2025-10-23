6906c1415deb8a71e55e83ea4b74c6c7adda75ec
likeyeon <chy98csy00@naver.com>
2025-10-23 10:46:57 +0900

    📦 Install and Set up Babel + Polyfill + Webpack for IE compatibility

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './js/swiper.js',
+    './js/visual-swiper.js',
+    './js/popup-swiper.js',
+    './js/banner-swiper.js',
+    './js/gnb-select.js',
+    './js/lnb-hover.js',
+    './js/drawer.js',
+    './js/drawer-nav.js',
+    './js/news-render.js',
+    './js/news-tab.js',
+    './js/scroll-top.js',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: { ie: '11' },
+                  useBuiltIns: 'usage',
+                  corejs: '3',
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  mode: 'development',
+  target: ['web', 'es5'],
+};
