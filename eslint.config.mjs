063af1aa369876fefd0664569fb0954764549c9e
likeyeon <chy98csy00@naver.com>
2025-10-25 12:15:41 +0900

    🎨 Improve structure using prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    env: {
+      browser: true,
+      commonjs: true,
+      es6: true,
+      node: true,
+    },
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
 ]);
